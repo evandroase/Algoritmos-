1a405f0913f6bfb0fe79f75f831bc9c9283645b1
evandroase <evandroase@gmail.com>
2020-07-20 16:27:13 -0300

    Exercicio 3

---
+++ Exercício 3/Exercício 3.pas
@@ -0,0 +1,34 @@
+//Exercício 3: Escreva um algoritmo que receba uma temperatura em graus celsius e coverta-a para graus Farenheit.
+// Exibir as duas temperaturas. Fórmula : Tf = (9/5) * Tc + 32.
+
+{
+																	Solução em Portugol
+
+Algoritmo Exercicio3;                   //Tc = temperatura em celsius
+Var                                     //Tf = temperatura em farenheit
+	Tf,Tc : real
+Inicio
+	exiba('Programa que converte temperaturas de Celsius para Farenheit.');
+	exiba('Digite uma temperatura em celsius: ');
+	leia(Tc);
+	Tf <- (9/5) * Tc + 32;
+	exiba(Tc,' º celsius é equivalente a ' ,Tf,' º farenheit.');
+Fim.
+
+}
+
+// Solução em Pascal
+
+Program Exercicio;
+uses crt;
+var
+	Tf,Tc : real;
+begin
+	clrscr;											 // Esse comando limpa a tela do terminal que abre quando executamos o programa.
+	writeln('Programa que converte temperaturas de Celsius para Farenheit.');
+	writeln('Digite uma temperatura em celsius: ');
+	readln(Tc);
+	Tf := (9/5) * Tc + 32;
+	writeln(Tc,' º celsius é equivalente a ' ,Tf,' º farenheit.');
+  repeat until keypressed;
+end.
---
+++ Formato base de solução.pas
@@ -0,0 +1,26 @@
+//Exercício : 
+
+{
+																	Solução em Portugol
+
+Algoritmo Exercicio;
+Var
+	
+Inicio
+  
+Fim.
+
+}
+
+// Solução em Pascal
+
+Program Exercicio;
+uses crt;
+
+var
+
+
+begin
+   
+    repeat until keypressed;
+end.
