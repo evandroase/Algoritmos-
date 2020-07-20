4347bb8a5a44d8b814c8609a1e46e3436c80daba
evandroase <evandroase@gmail.com>
2020-07-20 19:13:27 -0300

    Update Formato base de solução.pas

--- Formato base de solução.pas
+++ Formato base de solução.pas
@@ -1,29 +1,32 @@
 //Exercício : 
+//
 
 {		      Solução em Portugol
-
-Algoritmo Exercicio;
+Algoritmo Exercicio ;
+Const
+    ano = 2020; 
 Var
-	
+    ano_nascimento, idade: inteiro;
+    nome: caracter;
 Inicio
-  
-Fim.
+    exiba(" ");
+    leia( );
 
+Fim.
 }
 
 // Solução em Pascal
 
 Program Exercicio;
 uses crt;
-
+const
+    ano = 2020;
 var
-
-
+    ano_nascimento, idade: integer;
+    nome: string;
 begin
     clrscr;
-
-
-
-
+    writeln(' ');
+    readln();
     repeat until keypressed;
 end.
