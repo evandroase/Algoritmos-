5bb14c20910234c363e89b242dbcb3d7d75e1e40
evandroase <evandroase@gmail.com>
2020-07-20 18:18:08 -0300

    Update Exercício 5.pas

--- Exercício 5/Exercício 5.pas
+++ Exercício 5/Exercício 5.pas
@@ -5,13 +5,13 @@
 Algoritmo Exercicio;
 Var
     salario: inteiro;
-		salario_diario: real;
+    salario_diario: real;
 Inicio
     exiba("Programa que calcula o salário diário de um funcionário.");
-		exiba("Digite o salário do funcionário: ");
-		leia(salario);
-		salario_diario <- salario/30; // Calculo do salário de 1 dia de trabalho em um mês com 30 dias.
-		exiba("O salário diário do funcionário é: ", salario_diario);
+    exiba("Digite o salário do funcionário: ");
+    leia(salario);
+    salario_diario <- salario/30; // Calculo do salário de 1 dia de trabalho em um mês com 30 dias.
+    exiba("O salário diário do funcionário é: ", salario_diario);
 Fim.
 
 }
@@ -27,9 +27,9 @@ var
 begin
     clrscr;
     writeln('Programa que calcula o salário diário de um funcionário.');
-		writeln('Digite o salário do funcionário: ');
-		readln(salario);
-		salario_diario := salario/30; // Calculo do salário de 1 dia de trabalho em um mês com 30 dias.
-		writeln('O salário diário do funcionário é: ',salario_diario:9:2); //Comando para representar reais de maneira
+    writeln('Digite o salário do funcionário: ');
+    readln(salario);
+    salario_diario := salario/30; // Calculo do salário de 1 dia de trabalho em um mês com 30 dias.
+    writeln('O salário diário do funcionário é: ',salario_diario:9:2); //Comando para representar reais de maneira
 																																			 //mais fácil de ler:
 end.                                                                   //variavel:casas_antes_virgula:casas_depois_virg
