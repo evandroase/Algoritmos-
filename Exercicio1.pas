b877d8bafb6af760e25301ee5403dc3d95fa05fd
evandroase <evandroase@gmail.com>
2020-07-20 03:32:22 -0300

    Ex1_port+pascal

---
+++ Exercicio1.pas
@@ -0,0 +1,34 @@
+//Exercício 1: Escreva um algoritmo que receba um número qualquer e exiba o dobro desse número.
+
+{
+																	Solução em Portugol
+
+Algoritmo Exercicio1;
+Var
+	numero,dobro: inteiro;
+Inicio
+  exiba('Programa que dobra um número.');
+  exiba('Digite um número: ');
+  leia(numero);
+  dobro := numero * 2;
+  exiba('O dobro de ',numero,' é: ', dobro);
+Fim.
+
+}
+
+// Solução em Pascal
+
+Program Exercicio1;
+uses crt;
+
+var
+numero,dobro: integer;
+
+begin
+    writeln('Programa que dobra um número.');
+    writeln('Digite um número: ');
+    readln(numero);
+    dobro := numero * 2;
+    writeln('O dobro de ',numero,' é: ', dobro);
+    repeat until keypressed;    
+end.
