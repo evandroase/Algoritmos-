a70a842fe8e9d7848d514ef27a91f6ab8e44f3e4
evandroase <evandroase@gmail.com>
2020-07-21 20:21:02 -0300

    Exercício 13

---
+++ Estrutura Condicional Simples/Exercício 14.pas
@@ -0,0 +1,28 @@
+//Exercicio :
+
+{		      Solução em Portugol
+Algoritmo Exercicio ;
+Const
+	  const1 = 1;
+Var
+    var1: real;
+Inicio
+    exiba("");
+    leia();
+Fim.
+}
+
+// Solução em Pascal
+
+Program Exercicio;
+uses crt;
+const
+	  const1 = 1;
+var
+    var1: real;
+begin
+    clrscr;
+    writeln('');
+    readln();
+    repeat until keypressed;
+end.
--- Estruturas Sequenciais/Exercício 13/Exercício 13.pas
+++ Estruturas Sequenciais/Exercício 13/Exercício 13.pas
@@ -10,10 +10,10 @@ Inicio
     exiba("Programa que converte segundos em horas-minutos-segundos.");
     exiba("Digite um numero de segundos: ");
     leia(numero);
-		horas <- numero/3600;    --> estou usando como divisão inteira, no pascal isso não é possível.
-		minutos <- (numero - 3600 * horas)/60;
-		segundos <- (numero - 3600 * horas) - (minutos * 60);
-		exiba(numero," segundos equivalem a ", horas," horas, ",minutos," minutos e ",segundos," segundos.");
+    horas <- numero/3600;    --> estou usando como divisão inteira, no pascal isso não é possível.
+    minutos <- (numero - 3600 * horas)/60;
+    segundos <- (numero - 3600 * horas) - (minutos * 60);
+    exiba(numero," segundos equivalem a ", horas," horas, ",minutos," minutos e ",segundos," segundos.");
 Fim.
 }
 
@@ -28,9 +28,9 @@ begin
     writeln('Programa que converte segundos em horas-minutos-segundos.');
     writeln('Digite um numero de segundos: ');
     readln(numero);
-		horas := numero div 3600;
-		minutos := (numero mod 3600) div 60;
-		segundos := (numero mod 3600) mod 60;
-	  writeln(numero,' segundos equivalem a ', horas,' horas, ',minutos,' minutos e ',segundos,' segundos.');
+    horas := numero div 3600;
+    minutos := (numero mod 3600) div 60;
+    segundos := (numero mod 3600) mod 60;
+    writeln(numero,' segundos equivalem a ', horas,' horas, ',minutos,' minutos e ',segundos,' segundos.');
     repeat until keypressed;
 end.
