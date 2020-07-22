3aed0357a0db20c68817bfc1440cdc9214d92726
evandroase <evandroase@gmail.com>
2020-07-22 01:33:19 -0300

    Ex 15

--- Estrutura Condicional Simples/Exercício 15/Exercício 15.pas
+++ Estrutura Condicional Simples/Exercício 15/Exercício 15.pas
@@ -4,23 +4,23 @@
 {		      Solução em Portugol
 Algoritmo Exercicio 17;
 Const
-	valor_hora_normal = 10;
-	valor_hora_extra = 15;
+    valor_hora_normal = 10;
+    valor_hora_extra = 15;
 Var
-	salario_anual, hora_normal, hora_extra: real;
+    salario_anual, hora_normal, hora_extra: real;
 Inicio
-	exiba("Programa que calcula o salário anual de um trabalhador.");
-	exiba("Digite a quantidade de horas normais trabalhadas no ano: ");
-	leia(hora_normal);
-	exiba("Digite a quantidade de horas extras trabalhadas no ano: ");
-	leia(hora_extra);
-	salario_anual <- hora_normal * valor_hora_normal + hora_extra * valor_hora _extra;
-	se(salario_anual < 12000)
-		então exiba("O trabalhador deve pagar: ", salario_anual * 0,1," reais de imposto.");
-	fimse;
-	se(salario_anual >= 12000)
-		então exiba("O trabalhador deve pagar: ", 1200 + 0,25 * (salario_anual - 12000), " reais de imposto.");
-	fimse;
+    exiba("Programa que calcula o salário anual de um trabalhador.");
+    exiba("Digite a quantidade de horas normais trabalhadas no ano: ");
+    leia(hora_normal);
+    exiba("Digite a quantidade de horas extras trabalhadas no ano: ");
+    leia(hora_extra);
+    salario_anual <- hora_normal * valor_hora_normal + hora_extra * valor_hora _extra;
+    se(salario_anual < 12000)
+        então exiba("O trabalhador deve pagar: ", salario_anual * 0,1," reais de imposto.");
+    fimse;
+    se(salario_anual >= 12000)
+        então exiba("O trabalhador deve pagar: ", 1200 + 0,25 * (salario_anual - 12000), " reais de imposto.");
+    fimse;
 Fim.
 }
 
@@ -29,21 +29,21 @@ Fim.
 Program Exercicio17;
 uses crt;
 Const
-	valor_hora_normal = 10;
-	valor_hora_extra = 15;
+    valor_hora_normal = 10;
+    valor_hora_extra = 15;
 var
-	salario_anual, hora_normal, hora_extra: real;
+    salario_anual, hora_normal, hora_extra: real;
 begin
-	clrscr;
-	writeln('Programa que calcula o salário anual de um trabalhador.');
-	writeln('Digite a quantidade de horas normais trabalhadas no ano: ');
-	readln(hora_normal);
-	writeln('Digite a quantidade de horas extras trabalhadas no ano: ');
-	readln(hora_extra);
-	salario_anual := hora_normal * valor_hora_normal + hora_extra * valor_hora_extra;
-	if(salario_anual < 12000)
-		then writeln('O trabalhador deve pagar: ', (salario_anual * 0.1):0:2,' reais de imposto.');
-	if(salario_anual >= 12000)
-		then writeln('O trabalhador deve pagar: ', (1200 + 0.25 * (salario_anual - 12000)):0:2, ' reais de imposto.');
-	repeat until keypressed;
+    clrscr;
+    writeln('Programa que calcula o salário anual de um trabalhador.');
+    writeln('Digite a quantidade de horas normais trabalhadas no ano: ');
+    readln(hora_normal);
+    writeln('Digite a quantidade de horas extras trabalhadas no ano: ');
+    readln(hora_extra);
+    salario_anual := hora_normal * valor_hora_normal + hora_extra * valor_hora_extra;
+    if(salario_anual < 12000)
+        then writeln('O trabalhador deve pagar: ', (salario_anual * 0.1):0:2,' reais de imposto.');
+    if(salario_anual >= 12000)
+        then writeln('O trabalhador deve pagar: ', (1200 + 0.25 * (salario_anual - 12000)):0:2, ' reais de imposto.');
+    repeat until keypressed;
 end.
