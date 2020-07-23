9b1256035bb4ffeff06531ebefa98fce82d39ba3
evandroase <evandroase@gmail.com>
2020-07-23 01:18:19 -0300

    Update Exercício 24.pas

--- Operadores Especiais/Exercício 24/Exercício 24.pas
+++ Operadores Especiais/Exercício 24/Exercício 24.pas
@@ -1,7 +1,7 @@
 { Exercicio 24: Escreva um algoritmo que receba o nome e a idade de uma pessoa. Exibir o nome da pessoa e as
  seguintes expressões, conforme o caso:
  
-		Idade							Expressão
+Idade							Expressão
 Abaixo de 16 anos			Parabéns! Mas você é muito jovem.
 Entre 16 e 17 anos			Parabéns! Você já pode votar.
 Entre 18 e 21 anos			Parabéns! Você já pode votar e tirar carteira de motorista.
