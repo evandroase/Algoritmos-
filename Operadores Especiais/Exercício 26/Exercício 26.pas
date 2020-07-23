764a3877d48d2c33ad970f017869c308c4ba7e67
evandroase <evandroase@gmail.com>
2020-07-23 15:53:10 -0300

    Update Exercício 26.pas

--- Operadores Especiais/Exercício 26/Exercício 26.pas
+++ Operadores Especiais/Exercício 26/Exercício 26.pas
@@ -1,13 +1,13 @@
 {Exercicio 26:Escreva um algoritmo que receba o nome e a idade de um atleta para uma competição. No final, exibir um
 boletim constando o nome do atleta e sua respectiva categoria, baseando-se na tabela abaixo:
 
-Idade      Categoria
-Abaixo de 08 anos      Não pode participar!
+   Idade                    Categoria
+Abaixo de 08 anos       Não pode participar!
 Entre 08 e 10 anos      Pré-Mirim
 Entre 11 e 13 anos      Mirim
 Entre 14 e 16 anos      Infantil
 Entre 17 e 19 anos      Juvenil
-Acima de 19 anos      Veterano
+Acima de 19 anos        Veterano
 }
 {		      Solução em Portugol
 Algoritmo Exercicio ;
