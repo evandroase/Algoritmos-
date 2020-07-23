335fd29f27728a9fb78e92108a6e87845fc3c013
evandroase <evandroase@gmail.com>
2020-07-23 15:52:13 -0300

    Create Exercício 26.pas

---
+++ Operadores Especiais/Exercício 26/Exercício 26.pas
@@ -0,0 +1,37 @@
+{Exercicio 26:Escreva um algoritmo que receba o nome e a idade de um atleta para uma competição. No final, exibir um
+boletim constando o nome do atleta e sua respectiva categoria, baseando-se na tabela abaixo:
+
+Idade      Categoria
+Abaixo de 08 anos      Não pode participar!
+Entre 08 e 10 anos      Pré-Mirim
+Entre 11 e 13 anos      Mirim
+Entre 14 e 16 anos      Infantil
+Entre 17 e 19 anos      Juvenil
+Acima de 19 anos      Veterano
+}
+{		      Solução em Portugol
+Algoritmo Exercicio ;
+Const
+  const1 = 1;
+Var
+  var1: real;
+Inicio
+  exiba("");
+  leia();
+Fim.
+}
+
+// Solução em Pascal
+
+Program Exercicio;
+uses crt;
+const
+  const1 = 1;
+var
+  var1: real;
+begin
+  clrscr;
+  writeln('');
+  readln();
+  repeat until keypressed;
+end.
