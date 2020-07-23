e644d283ad8a11a031e5a2ecf0113b6291206c2b
evandroase <evandroase@gmail.com>
2020-07-23 15:14:29 -0300

    Exercício 25

---
+++ Operadores Especiais/Exercício 25/Exercício 25.pas
@@ -0,0 +1,100 @@
+//Exercicio 25: Escreva um algoritmo que leia três números e exiba-os em ordem crescente.
+
+{		      Solução em Portugol
+Algoritmo Exercicio 25;
+Var
+  N1,N2,N3,maior: real;
+Inicio
+  exiba("Programa que coloca 3 números em ordem crescente.");
+  exiba("Digite o primeiro número: ");
+  leia(N1);
+  exiba("Digite o segundo número: ");
+  leia(N2);                                      //Existem 13 possibilidades, sendo que cada uma é avaliada por um
+  exiba("Digite o terceiro número: ");           //condicional diferente.
+  leia(N3);
+  se((N1 > N2) e (N2 > N3))                     //N3 < N2 < N1
+    então exiba(N3,"-",N2,"-",N3);
+  fimse;
+  se((N2 > N1) e (N1 > N3))                     //N3 < N1 < N2
+    então exiba(N3,"-",N1,"-",N2);
+  fimse;
+  se((N3 > N1) e (N1 > N2))                     //N2 < N1 < N3
+    então exiba(N2,"-",N1,"-",N3);
+  fimse;
+  se((N1 > N3) e (N3 > N2))                     //N2 < N3 < N1
+    então exiba(N2,"-",N3,"-",N1);
+  fimse;
+  se((N3 > N2) e (N2 > N1))                     //N1 < N2 < N3
+    então exiba(N1,"-",N2,"-",N3);
+  fimse;
+  se((N2 > N3) e (N3> N1))                      //N1 < N3 < N2
+    então exiba(N1,"-",N3,"-",N2);
+  fimse;
+  se((N2 = N3) e (N3 = N1))                     //N1 = N3 = N2
+    então exiba(N1,"-",N3,"-",N2);
+  fimse;
+  se((N2 = N3) e (N1 > N2))                     //N2 = N3 < N1
+    então exiba(N2,"-",N3,"-",N1);
+  fimse;
+  se((N2 = N3) e (N1 < N2))                     //N2 = N3 > N1
+    então exiba(N1,"-",N3,"-",N2);
+  fimse;
+  se((N1 = N3) e (N1 > N2))                     //N1 = N3 > N2
+    então exiba(N2,"-",N3,"-",N1);
+  fimse;
+  se((N1 = N3) e (N1 < N2))                     //N1 = N3 < N2
+    então exiba(N1,"-",N3,"-",N2);
+  fimse;
+  se((N2 = N1) e (N2 < N3))                     //N1 = N2 < N3
+    então exiba(N2,"-",N1,"-",N3);
+  fimse;
+  se((N2 = N1) e (N2 > N3))                     //N1 = N2 > N3
+    então exiba(N3,"-"N2,"-",N1);
+  fimse;
+
+Fim.
+}
+
+// Solução em Pascal
+
+Program Exercicio25;
+uses crt;
+var
+  N1,N2,N3: integer;
+begin
+  clrscr;
+  writeln('Programa que coloca 3 números em ordem crescente.');
+  writeln('Digite o primeiro número: ');
+  readln(N1);
+  writeln('Digite o segundo número: ');           //Existem 13 possibilidades, sendo que cada uma é avaliada por um
+  readln(N2);                                     //condicional diferente.
+  writeln('Digite o terceiro número: ');
+  readln(N3);
+  if((N1 > N2) and (N2 > N3))                     //N3 < N2 < N1
+    then writeln(N3,'-',N2,'-',N1);
+  if((N2 > N1) and (N1 > N3))                     //N3 < N1 < N2
+    then writeln(N3,'-',N1,'-',N2);
+  if((N3 > N1) and (N1 > N2))                     //N2 < N1 < N3
+    then writeln(N2,'-',N1,'-',N3);
+  if((N1 > N3) and (N3 > N2))                     //N2 < N3 < N1
+    then writeln(N2,'-',N3,'-',N1);
+  if((N3 > N2) and (N2 > N1))                     //N1 < N2 < N3
+    then writeln(N1,'-',N2,'-',N3);
+  if((N2 > N3) and (N3> N1))                      //N1 < N3 < N2
+    then writeln(N1,'-',N3,'-',N2);
+  if((N2 = N3) and (N3 = N1))                     //N1 = N3 = N2
+    then writeln(N1,'-',N3,'-',N2);
+  if((N2 = N3) and (N1 > N2))                     //N2 = N3 < N1
+    then writeln(N2,'-',N3,'-',N1);
+  if((N2 = N3) and (N1 < N2))                     //N2 = N3 > N1
+    then writeln(N1,'-',N3,'-',N2);
+  if((N1 = N3) and (N1 > N2))                     //N1 = N3 > N2
+    then writeln(N2,'-',N3,'-',N1);
+  if((N1 = N3) and (N1 < N2))                     //N1 = N3 < N2
+    then writeln(N1,'-',N3,'-',N2);
+  if((N2 = N1) and (N2 < N3))                     //N1 = N2 < N3
+    then writeln(N2,'-',N1,'-',N3);
+  if((N2 = N1) and (N2 > N3))                     //N1 = N2 > N3
+    then writeln(N3,'-',N2,'-',N1);
+  repeat until keypressed;
+end.
