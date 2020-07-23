fb89e852715ccfcf6626f4bc730061514b173790
evandroase <evandroase@gmail.com>
2020-07-23 01:17:36 -0300

    Enunciado do 24

---
+++ Operadores Especiais/Exercício 24/Exercício 24.pas
@@ -0,0 +1,37 @@
+{ Exercicio 24: Escreva um algoritmo que receba o nome e a idade de uma pessoa. Exibir o nome da pessoa e as
+ seguintes expressões, conforme o caso:
+ 
+		Idade							Expressão
+Abaixo de 16 anos			Parabéns! Mas você é muito jovem.
+Entre 16 e 17 anos			Parabéns! Você já pode votar.
+Entre 18 e 21 anos			Parabéns! Você já pode votar e tirar carteira de motorista.
+Acima de 21 anos			Parabéns! Você está ficando experiente.
+}
+
+
+{		      Solução em Portugol
+Algoritmo Exercicio 24;
+Const
+  const1 = 1;
+Var
+  var1: real;
+Inicio
+  exiba("");
+  leia();
+Fim.
+}
+
+// Solução em Pascal
+
+Program Exercicio24;
+uses crt;
+const
+  const1 = 1;
+var
+  var1: real;
+begin
+  clrscr;
+  writeln('');
+  readln();
+  repeat until keypressed;
+end.
