88434469f7e767b9c0f4d09ef1f3473848b09c68
evandroase <evandroase@gmail.com>
2020-07-24 13:09:01 -0300

    Create Exercício 34.pas

---
+++ Estrutura Condicional Encadeada/Exercício 34/Exercício 34.pas
@@ -0,0 +1,29 @@
+//Exercicio :
+
+{		      Solução em Portugol
+Algoritmo Exercicio ;
+Const
+  const1 = 1;
+Var
+  var1: real;
+Inicio
+  exiba("");
+  exiba("");
+  leia();
+Fim.
+}
+
+// Solução em Pascal
+
+Program Exercicio;
+uses crt;
+const
+  const1 = 1;
+var
+  var1: real;
+begin
+  clrscr;
+  writeln('');
+  readln();
+  repeat until keypressed;
+end.
