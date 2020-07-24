7ac7f15ff58697dae1be3d7ef48edeaae3c252bc
evandroase <evandroase@gmail.com>
2020-07-24 19:30:41 -0300

    Create Exercício 38.pas

---
+++ Estruturas de Repetição - I/Exercício 38/Exercício 38.pas
@@ -0,0 +1,45 @@
+//Exercicio 38: Escreva um algoritmo que receba 20 valores reais e exiba o seu somatório.
+
+{		      Solução em Portugol
+Algoritmo Exercicio 38;
+Var
+  contador: inteiro;
+  N,soma: real;
+Inicio
+  exiba("Programa que calcula o somatório de 20 números.");
+  contador <- 1;
+  soma <- 0;
+  enquanto(contador < 21) faca
+	Inicio
+    exiba("Digite o ",contador,"º número.");
+    leia(N);
+    soma <- soma + N;
+    contador <- contador + 1;
+  Fim;
+  exiba("O valor do somatório dos 20 números é: ",soma);
+Fim.
+}
+
+// Solução em Pascal
+
+Program Exercicio38;
+uses crt;
+var
+  contador: integer;
+  N,soma: real;
+begin
+  clrscr;
+  writeln('Programa que calcula o somatório de 20 números.');
+  contador := 1;
+  soma := 0;
+  while(contador < 21) do
+	Begin
+    writeln('Digite o ',contador,'º número.');
+    readln(N);
+    soma := soma + N;
+    contador :=
+	  contador + 1;
+  End;
+  writeln('O valor do somatório dos 20 números é: ',soma:0:2);
+  repeat until keypressed;
+end.
