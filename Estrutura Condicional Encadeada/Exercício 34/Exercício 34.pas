7705821f825346adf50c64498b4ed7e0c7b45b37
evandroase <evandroase@gmail.com>
2020-07-24 13:35:38 -0300

    Update Exercício 34.pas

--- Estrutura Condicional Encadeada/Exercício 34/Exercício 34.pas
+++ Estrutura Condicional Encadeada/Exercício 34/Exercício 34.pas
@@ -1,4 +1,7 @@
-//Exercicio 34: Escreva um algoritmo que receba os três coeficientes de uma Equação do //2º Grau e calcule e exiba a(s) raiz(es) da equação, se existir(em). Se não existir, ///informar ao usuário. Caso o coeficiente “a” for igual à zero, informar que não se /////trata de uma equação do segundo grau e encerre o algoritmo.
+//Exercicio 34: Escreva um algoritmo que receba os três coeficientes de uma Equação do
+//2º Grau e calcule e exiba a(s) raiz(es) da equação, se existir(em). Se não existir,
+//informar ao usuário. Caso o coeficiente “a” for igual à zero, informar que não se
+//trata de uma equação do segundo grau e encerre o algoritmo.
 
 {		      Solu��o em Portugol
 Algoritmo Exercicio 34;
@@ -33,23 +36,21 @@ uses crt;
 var
   a,b,c,delta: real;
 begin
-  writeln("Programa que resolve equações do segundo grau.");
-  writeln("Digite o valor do coeficiente a: ");
-  leia(a);
-  writeln("Digite o valor do coeficiente b: ");
-  leia(b);
-  writeln("Digite o valor do coeficiente c: ");
-  leia(c);
-  delta <- b*b - 4*a*c;
-  se(a = 0)
-    then writeln("A equação não é do segundo grau.")
-    else if(delta < 0)
-            then writeln("A equação não tem raízes reais.")
-            else if(delta = 0)
-                    then writeln("Raízes iguais e que valem: ", -b/(2*a))
-                    else writeln("Raiz 1: ", (-b + (delta)^0.5)/(2*a),"Raiz 2: ", (-b - (delta)^0.5)/(2*a));
   clrscr;
-  writeln('');
-  readln();
+  writeln('Programa que resolve equações do segundo grau.');
+  writeln('Digite o valor do coeficiente a: ');
+  readln(a);
+  writeln('Digite o valor do coeficiente b: ');
+  readln(b);
+  writeln('Digite o valor do coeficiente c: ');
+  readln(c);
+  delta := b*b - 4*a*c;
+  if(a = 0)
+    then writeln('A equação não é do segundo grau.')
+    else if(delta < 0)
+            then writeln('A equação não tem raízes reais.')
+            else if(delta = 0)
+                    then writeln('Raízes iguais e que valem: ', -b/(2*a))
+                    else writeln('Raiz 1: ', ((-b + (exp(0.5*ln(delta))))/(2*a)):0:2,' - Raiz 2: ', ((-b - (exp(0.5*ln(delta))))/(2*a)):0:2);
   repeat until keypressed;
 end.
