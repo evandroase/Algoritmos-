32c38dbf927be018e5a603d5074274d212f22c00
evandroase <evandroase@gmail.com>
2020-07-31 23:29:14 -0300

    Exercício 99

--- Rotinas/Exercício 99/Exercício 99.pas
+++ Rotinas/Exercício 99/Exercício 99.pas
@@ -4,31 +4,34 @@
 Algoritmo Exercicio 99;
 Var
   H,M,S,RESULTADO: inteiro;
-Procedimento Conversor(h: inteiro; m: inteiro; s: inteiro;Var resultado: inteiro);
+Procedimento ConversorSegundos(aux: inteiro;Var resultado: inteiro);
+Var
+  h,m,s: inteiro;
 Inicio
+  exiba("Digite um número de horas:");
+  leia(h);
+  enquanto(h < 0)faça
+    exiba("Digite um número de horas válido.");
+    leia(h);
+  fimenquanto;
+  exiba("Digite um número de minutos:");
+  leia(m);
+  enquanto(m < 0)faça
+    exiba("Digite um número de minutos válido.");
+    leia(m);
+  fimenquanto;
+  exiba("Digite um número de segundos:");
+  leia(s);
+  enquanto(s < 0)faça
+    exiba("Digite um número de segundos válido.");
+    leia(s);
+  fimenquanto;
   resultado <- h * 3600 + m * 60 + s;
 Fim;
 Inicio
+  Aux = 0;
   exiba("Programa que converte Horas, Minutos, Segundos em Segundos.");
-  exiba("Digite um número de horas:");
-  leia(H);
-  enquanto(H < 0)faça
-    exiba("Digite um número de horas válido.");
-    leia(H);
-  fimenquanto;
-  exiba("Digite um número de minutos:");
-  leia(M);
-  enquanto(M < 0)faça
-    exiba("Digite um número de minutos válido.");
-    leia(M);
-  fimenquanto;
-  exiba("Digite um número de segundos:");
-  leia(S);
-  enquanto(S < 0)faça
-    exiba("Digite um número de segundos válido.");
-    leia(S);
-  fimenquanto;
-  Conversor(H,M,S,RESULTADO);
+  ConversorSegundos(Aux,RESULTADO);
   exiba(H," horas, ",M," minutos e ",S," segundos equivalem a ", RESULTADO," segundos.");
 Fim.
 }
@@ -38,35 +41,39 @@ Fim.
 Program Exercicio99;
 uses crt;
 Var
-  H,M,S,RESULTADO: integer;
-Procedure Conversor(h: integer; m: integer; s: integer;Var resultado: integer);
+  Aux,RESULTADO: integer;
+Procedure ConversorHorario(aux: integer; Var resultado: integer);
+Var
+ h,m,s: integer;
 Begin
-  resultado := h * 3600 + m * 60 + s;
-End;
-Begin
-  writeln('Programa que converte Horas, Minutos, Segundos em Segundos.');
   writeln('Digite um número de horas:');
-  readln(H);
-  while(H < 0)do
+  readln(h);
+  while(h < 0)do
   Begin
     writeln('Digite um número de horas válido.');
-    readln(H);
+    readln(h);
   End;
   writeln('Digite um número de minutos:');
-  readln(M);
-  while(M < 0)do
+  readln(m);
+  while(m < 0)do
   Begin
     writeln('Digite um número de minutos válido.');
-    readln(M);
+    readln(m);
   End;
   writeln('Digite um número de segundos:');
-  readln(S);
-  while(S < 0)do
+  readln(s);
+  while(s < 0)do
   Begin
     writeln('Digite um número de segundos válido.');
-    readln(S);
+    readln(s);
   End;
-  Conversor(H,M,S,RESULTADO);
-  writeln(H,' horas, ',M,' minutos e ',S,' segundos equivalem a ', RESULTADO,' segundos.');
+  resultado := h * 3600 + m * 60 + s;
+  write(h,' horas, ',m,' minutos e ',s,' segundos equivalem a ');
+End;
+Begin
+  Aux := 0;
+  writeln('Programa que converte Horas, Minutos, Segundos em Segundos.');
+  ConversorHorario(Aux,RESULTADO);
+  writeln(RESULTADO,' segundos.');
   repeat until keypressed;
 end.
