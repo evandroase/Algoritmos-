a1e3c3ec3d1ead47da0e2458b148d9eb59d7bbdb
evandroase <evandroase@gmail.com>
2020-07-31 21:05:19 -0300

    Exercício 100

--- Rotinas/Exercício 100/Exercício 100.pas
+++ Rotinas/Exercício 100/Exercício 100.pas
@@ -17,7 +17,7 @@ Inicio
   exiba("Programa que converte de Celsius para Kelvin.");
   exiba("Digite uma temperatura em Celsius:");
   leia(C);
-	Conversor(C,K);
+  Conversor(C,K);
   exiba(C," Celsius enquivale a ",K," Kelvin.");
 Fim.
 }
@@ -41,7 +41,7 @@ Begin
   writeln('Programa que converte de Celsius para Kelvin.');
   writeln('Digite uma temperatura em Celsius:');
   readln(C);
-	Conversor(C,K);
+  Conversor(C,K);
   writeln(C:0:2,' Celsius enquivale a ',K:0:2,' Kelvin.');
   repeat until keypressed;
 end.
